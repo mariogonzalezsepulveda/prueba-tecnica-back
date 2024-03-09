5fa98efee06e845f001c8bf54c6818c0ab96903c
Mario Gonzalez Sepulveda <mariogonzalezsepulveda@gmail.com>
2024-03-09 18:13:42 -0300

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # prueba-tecnica-back
+Prueba Tecnica para 3IT
+
+<!-- TOC -->
+* JDK 17
+* Spring Boot  3.2.3
+<!-- TOC -->
+
+
+| H2 |
+|----|
+| http://localhost:8001/db   |
+| jdbc url jdbc:h2:mem:evaluaciondb   |
+
+En resources se añade consulta SQL para llenar registros
+
+
+
---
+++ src/main/resources/dataParaRegistro.sql
@@ -0,0 +1,20 @@
+INSERT INTO REGISTRO (email, seleccion)
+VALUES
+    ('usuario1@dominio.com', 'Pop'),
+    ('usuario2@dominio.com', 'Rock'),
+    ('usuario3@dominio.com', 'Electro'),
+    ('usuario4@dominio.com', 'Jazz'),
+    ('usuario5@dominio.com', 'Clasica'),
+    ('usuario6@dominio.com', 'Clasica'),
+    ('usuario7@dominio.com', 'Clasica'),
+    ('usuario8@dominio.com', 'Rock'),
+    ('usuario9@dominio.com', 'Rock'),
+    ('usuario10@dominio.com', 'Rock'),
+    ('usuario11@dominio.com', 'Rock'),
+    ('usuario12@dominio.com', 'Rock'),
+    ('usuario15@dominio.com', 'Pop'),
+    ('usuario16@dominio.com', 'Pop'),
+    ('usuario17@dominio.com', 'Jazz'),
+    ('usuario18@dominio.com', 'Jazz'),
+    ('usuario19@dominio.com', 'Electro'),
+    ('usuario20@dominio.com', 'Pop');
